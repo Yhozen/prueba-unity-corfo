fcaf9178592a92740ad03452ff22e872ad162ef9
Gabriel P??rez <gabriel@garox.org>
2022-01-30 10:54:26 -0300

    use points on ui

--- Assets/Scripts/UIDialog.cs
+++ Assets/Scripts/UIDialog.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
+
 public class UIDialog : MonoBehaviour
 {
     // Start is called before the first frame update
     public Text TextObject;
     public TextMeshProUGUI points;
+    public PointsManager pointsManager;
+
     void Start()
     {
         TextObject.text = "test";
@@ -18,6 +21,6 @@ public class UIDialog : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        points.text = $"Points: {pointsManager.getCurrentPoints()}";
     }
 }
