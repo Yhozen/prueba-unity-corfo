9e8ffead434691eb64bf055fa3c0cd699d07f0dd
Gabriel P??rez <gabriel@garox.org>
2022-01-30 10:51:45 -0300

    use TextMesh Pro

--- Assets/Scripts/UIDialog.cs
+++ Assets/Scripts/UIDialog.cs
@@ -2,13 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+
 public class UIDialog : MonoBehaviour
 {
     // Start is called before the first frame update
     public Text TextObject;
+    public TextMeshProUGUI points;
     void Start()
     {
         TextObject.text = "test";
+        points.text = "ala";
     }
 
     // Update is called once per frame
